879072a884fb9e86853be1dd96c6a44d7168b283
rifkijones <rifki.jones@yahoo.co.uk>
2020-09-08 11:13:20 +0100

    React navbar and routes boilerplate

---
+++ src/components/navigation.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const navLinks = [
+  {
+    title: 'Home',
+    path: '/'
+  },
+  {
+    title: 'Blog',
+    path: '/blog'
+  },
+  {
+    title: 'Contact us',
+    path: '/contact-us'
+  },
+  {
+    title: 'Login',
+    path: '/login'
+  }
+];
+export default function Navigation() {
+  return (
+    <nav className="site-navigation">
+      <span>React blog</span>
+      <ul>
+        {navLinks.map((link, index) => (
+          <li key={index}>
+            <Link to={link.path}>{link.title}</Link>
+          </li>
+        ))}
+      </ul>
+    </nav>
+  );
+}
